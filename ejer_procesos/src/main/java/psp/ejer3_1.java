af81c63aa7b89dddd20fa6cb2db6423f5f0853ea
khrixthian <cristian.fernandezro@elorrieta-errekamari.com>
2020-10-05 17:27:14 +0200

    ejer3_1

---
+++ ejer_procesos/src/main/java/psp/ejer3_1.java
@@ -0,0 +1,14 @@
+package psp;
+
+import java.util.Scanner;
+
+public class ejer3_1 {
+
+	public void EscribirTexto() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Dame un texto");
+			 String texto = sc.nextLine();
+		
+	}
+}
