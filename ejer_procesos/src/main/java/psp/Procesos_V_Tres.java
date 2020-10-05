33088fb923f4e340b346c918b38c8050a5f9aa8f
khrixthian <cristian.fernandezro@elorrieta-errekamari.com>
2020-10-05 21:21:35 +0200

    ejer frame1, pero no va

---
+++ ejer_procesos/src/main/java/psp/Procesos_V_Tres.java
@@ -0,0 +1,33 @@
+package psp;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Frame1 {
+
+	public static void main(String[] args) {
+		//Haz un programa llamado Frame1 que lance una ventana con un botón. Al pulsarlo finaliza el
+		//programa.
+		//Haz otro programa llamado Procesos_V_Tres que lance Frame1 y espere hasta que finalice,
+		//cuando lo haga mostrará por consola el valor devuelto por Frame1.
+		
+		ProcessBuilder pb = new ProcessBuilder();
+		
+		pb.directory(new File(".//bin"));
+		pb = pb.command("java","ejer 12");
+		
+		try {
+			Process pro = pb.start();
+			int valorSalida = pro.waitFor();
+			
+		}catch(IOException e){
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
