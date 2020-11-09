36dc667f84b188d76935e37f81f35ccb4b736715
khrixthian <cristian.fernandezro@elorrieta-errekamari.com>
2020-11-09 14:24:38 +0100

    lunes 9-11 klase2

---
+++ ejer_procesos/src/main/java/BolsaProducto/Bolsa.java
@@ -0,0 +1,12 @@
+package BolsaProducto;
+
+import java.util.ArrayList;
+
+public class Bolsa extends Thread {
+	ArrayList<Producto> listaproductos;
+	Producto prod = new Producto("producto");
+
+	public void addProducto(Producto prod) {
+
+	}
+}
---
+++ ejer_procesos/src/main/java/BolsaProducto/Producto.java
@@ -0,0 +1,18 @@
+package BolsaProducto;
+
+public class Producto {
+	String nombre;
+
+	public Producto(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
